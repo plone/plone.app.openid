0aa2a3524a395ab8970d96d700f8a3eb95db2769
Wichert Akkerman <wichert@wiggy.net>
2006-12-26 13:11:10 +0000

    Add a browser view test for openid-only sites
    svn path=/plone.app.openid/trunk/; revision=11721

--- plone/app/openid/tests/test_view.py
+++ plone/app/openid/tests/test_view.py
@@ -1,5 +1,7 @@
 import unittest
 from plone.app.openid.tests.base import OpenIdTestCase
+from Products.PluggableAuthService.interfaces.plugins \
+        import IExtractionPlugin, ILoginPasswordExtractionPlugin
 
 class TestOpenIdView(OpenIdTestCase):
     def test_DefaultConfig(self):
@@ -7,13 +9,22 @@ class TestOpenIdView(OpenIdTestCase):
         self.assertEquals(view.ShowOpenIdLogin(), False)
         self.assertEquals(view.ShowStandardLogin(), True)
 
-    def test_Install(self):
+    def test_OpenIdInstalled(self):
         self.portal.portal_quickinstaller.installProduct("plone.app.openid")
         view=self.view
         self.assertEquals(view.ShowOpenIdLogin(), True)
         self.assertEquals(view.ShowStandardLogin(), True)
 
+    def testOnlyOpenIdInstalled(self):
+        plugins=self.pas.plugins.listPlugins(IExtractionPlugin)
+        for (id, plugin) in plugins:
+            if ILoginPasswordExtractionPlugin.providedBy(plugin):
+                plugin.manage_activateInterfaces(interfaces=())
+        self.portal.portal_quickinstaller.installProduct("plone.app.openid")
 
+        view=self.view
+        self.assertEquals(view.ShowOpenIdLogin(), True)
+        self.assertEquals(view.ShowStandardLogin(), False)
 
 
 def test_suite():
