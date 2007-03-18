07fea745e20ac4e015eee816073d04c5c1d46957
Hanno Schlichting <hanno@hannosch.eu>
2007-03-18 14:33:23 +0000

    Register the extension profile in zcml, otherwise the code in the initialize will be called twice, thx to the five:registerPackage call
    svn path=/plone.app.openid/trunk/; revision=13748

--- plone/app/openid/__init__.py
+++ plone/app/openid/__init__.py
@@ -1,18 +1,1 @@
-from Products.GenericSetup.interfaces import EXTENSION
-from Products.CMFPlone.interfaces import IPloneSiteRoot
-from Products.GenericSetup import profile_registry
-from plone.openid.config import HAS_OPENID
-
-def initialize(context):
-    if HAS_OPENID:
-        profile_registry.registerProfile(
-                name="default",
-                title="OpenID authentication support",
-                description="Adds support for authenticating with OpenID credentials in a Plone site",
-                path="profiles/default",
-                product="plone.app.openid",
-                profile_type=EXTENSION,
-                for_=IPloneSiteRoot)
-
-
-
+#
