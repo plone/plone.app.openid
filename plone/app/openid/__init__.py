613b665228a3460ac363ec842f9543f5c9a601ab
Wichert Akkerman <wichert@wiggy.net>
2007-07-08 11:23:23 +0000

    Use a zcml conditional instead of python code
    svn path=/plone.app.openid/trunk/; revision=15838

--- plone/app/openid/__init__.py
+++ plone/app/openid/__init__.py
@@ -1,23 +1,2 @@
-from Products.GenericSetup.interfaces import EXTENSION
-from Products.CMFPlone.interfaces import IPloneSiteRoot
-from Products.GenericSetup import profile_registry
-from plone.openid.config import HAS_OPENID
-
-def initialize(context):
-    if HAS_OPENID:
-        try:
-            profile_registry.registerProfile(
-                    name="default",
-                    title="OpenID Authentication Support",
-                    description="Adds support for authenticating with OpenID credentials in a Plone site",
-                    path="profiles/default",
-                    product="plone.app.openid",
-                    profile_type=EXTENSION,
-                    for_=IPloneSiteRoot)
-        except KeyError:
-            # The testrunner runs initialize for packages twice if they are
-            # registered with five:registerPackage, so we need to protect
-            # ourselves from registering our profile twice.
-            pass
-
+# Poof! And it was a package
 
