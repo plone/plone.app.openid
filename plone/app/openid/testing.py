2c05ef1a204267d30a417d0fe777e0b3582b35fc
Timo Stollenwerk <tisto@plone.org>
2015-03-11 09:32:40 +0100

    No need to install plone.app.openid as z2 pkg.

--- plone/app/openid/testing.py
+++ plone/app/openid/testing.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-
 from zope.configuration import xmlconfig
-
-from plone.testing import z2
-
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing.layers import IntegrationTesting
 
@@ -13,16 +9,13 @@ class PloneAppOpenidLayer(PloneSandboxLayer):
     def setUpZope(self, app, configurationContext):
         import plone.openid
         self.loadZCML(package=plone.openid)
-        z2.installProduct(app, 'plone.openid')
-        z2.installProduct(app, 'plone.app.openid')
         import plone.app.openid
         xmlconfig.file(
             'configure.zcml',
             plone.app.openid,
-            context=configurationContext)
+            context=configurationContext
+        )
 
-    def tearDownZope(self, app):
-        z2.uninstallProduct(app, 'plone.app.openid')
 
 PLONEAPPOPENID_FIXTURE = PloneAppOpenidLayer()
 
